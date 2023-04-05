063e6b087449592042cedf0e19f8d9a0b8e1f74b
Kamesh Balachandran <129968941+Kamesh-B@users.noreply.github.com>
2023-04-05 11:04:00 -0500

    Add files via upload

---
+++ Battery_percent.ino
@@ -0,0 +1,14 @@
+#include <ArduinoBLE.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int x = 1;
+  Serial.print(x);
+
+}
